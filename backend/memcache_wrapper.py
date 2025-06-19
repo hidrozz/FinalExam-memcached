2c50c9fdac302b1e18e186c67f12237f35d1e6d7
Hidayat Nur Ali Razzaq <91576030+hidrozz@users.noreply.github.com>
2025-06-19 21:37:59 +0700

    Update memcache_wrapper.py

--- backend/memcache_wrapper.py
+++ backend/memcache_wrapper.py
@@ -1,31 +1,8 @@
-from pymemcache.client.base import Client
-import json
+import csv
+from datetime import datetime
 
-client = Client(('localhost', 11211))
-
-def get(key):
-    value = client.get(key)
-    return value.decode() if value else None
-
-def set(key, value, expire=300):
-    if isinstance(value, dict):
-        value = json.dumps(value)
-    client.set(key, value, expire)
-
-def push_to_list(key, item, max_length=100):
-    list_raw = get(key)
-    try:
-        data = json.loads(list_raw) if list_raw else []
-        data.insert(0, item)
-        if len(data) > max_length:
-            data = data[:max_length]
-        client.set(key, json.dumps(data))
-    except:
-        client.set(key, json.dumps([item]))
-
-def get_list(key):
-    list_raw = get(key)
-    try:
-        return json.loads(list_raw) if list_raw else []
-    except:
-        return []
+def log_latency(platform, published, received, stored, read):
+    filename = f"latency_{platform.lower()}.csv"
+    with open(filename, mode="a", newline="") as file:
+        writer = csv.writer(file)
+        writer.writerow([datetime.now().isoformat(), published, received, stored, read])
