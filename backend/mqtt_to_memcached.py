0458df0fb7e13622bff8c060abcde04d6934c61e
Hidayat Nur Ali Razzaq <91576030+hidrozz@users.noreply.github.com>
2025-06-20 22:33:58 +0700

    Update mqtt_to_memcached.py

--- backend/mqtt_to_memcached.py
+++ backend/mqtt_to_memcached.py
@@ -3,18 +3,17 @@ import memcache
 import json
 from datetime import datetime
 
-# Konfigurasi MQTT (Broker di VPS 1)
-MQTT_HOST = "103.49.239.121"
+MQTT_HOST = "103.76.120.64"
 MQTT_PORT = 1883
 MQTT_USER = "myuser"
 MQTT_PASS = "tugasakhir"
 MQTT_TOPIC = "sensors/report"
 
-# Konfigurasi Memcached (lokal atau remote)
 mc = memcache.Client(["127.0.0.1:11211"])
 mc.behaviors = {"tcp_nodelay": True, "ketama": True}
 
-KEY_LOG = "sensor_data_log_m"
+KEY_DATA = "memcached:latest_sensor_data"
+KEY_LOG = "memcached:sensor_data_log"
 MAX_LOG = 100
 
 def on_connect(client, userdata, flags, rc):
@@ -27,13 +26,13 @@ def on_message(client, userdata, msg):
         data = json.loads(payload)
         data["timestamp"] = datetime.now().isoformat()
 
-        # Simpan data ke Memcached
-        mc.set("latest_sensor_data_m", json.dumps(data))
+        mc.set(KEY_DATA, json.dumps(data), time=0)
 
-        # Simpan ke log (list)
-        logs = mc.get(KEY_LOG) or []
+        logs = mc.get(KEY_LOG)
+        if not isinstance(logs, list):
+            logs = []
         logs.insert(0, data)
-        mc.set(KEY_LOG, logs[:MAX_LOG])
+        mc.set(KEY_LOG, logs[:MAX_LOG], time=0)
 
         print(f"[MQTT->Memcached] Stored: {data}")
 
